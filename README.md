26777a56cddf5539e3d8b9082dd7781e55ef2919
Lewis Prescott <Lewis.Prescott@cancer.org.uk>
2021-03-04 08:40:09 +0000

    Technical task

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# SeleniumDockerSample
-This tutorial dockerize the entire Selenium Project and run it inside the container. Anybody can run the program anywhere without cloning the repository, installing the dependencies, etc.
+# UI Tests
+
+Your task is to create automated checks for the search function for the CRUK find a shop page ([https://www.cancerresearchuk.org/get-involved/find-a-shop](https://www.cancerresearchuk.org/get-involved/find-a-shop)).
+
+## Scenarios
+
+You need to test the following scenarios (this is a live website, please don't overuse the page/functionality):
+
+1. create a "happy path" scenario.
+1. create a negative scenario.
+
+## Guidance
+
+If you prefer to use a different test framework (feel free to add it as a dependency, we use Cypress.io at CRUK) to solve the challenge. Use best practices for the test architecture and setup.
+
+### Setup
+1. Download ChromeDriver & put in `C:/chromedriver/` folder [Chromedriver Downloads](http://chromedriver.chromium.org/downloads). If not using windows you may need to change the executable path.
+1. Download [docker](https://docs.docker.com/get-docker/)
+
+### Run Selenium Grid
+Using docker to run Selenium Grid (Feel free to use a local instance yourself)
+```sh
+docker pull elgalu/selenium
+
+docker run -d --name=grid -p 4444:24444 -p 5900:25900 \
+     -e TZ="US/Pacific" -v /dev/shm:/dev/shm --privileged elgalu/selenium
+```
+
+### Run
+```sh
+mvn clean test -DsuiteXmlFile=testng/testng.xml
+```
+
+## Documentation
+
+Please describe in a README.md file how to run the tests and any other additional info needed about any assumptions you make.
+
+## On Completion
+
+Please either:
+1. Zip up the project and send it back to the hiring manager
+1. Publish to your github and send us the link
---
+++ src/test/java/shops/Search.java
@@ -0,0 +1,43 @@
+package shops;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
+
+public class Search {
+
+    // Task 1: finish this test...
+    // Task 2: Add negative scenario
+    // Task 3: Refactor me
+    @Test(enabled = true)
+    public void searchTest() throws Throwable {
+        // Setup
+        System.setProperty("webdriver.chrome.driver","C:/chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.cancerresearchuk.org/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+        driver.findElement(By.linkText("Find local shops")).click();
+        Assert.assertEquals(driver.findElement(By.tagName("h1")).getText(), "Shop search results");
+
+        WebElement breadcrumb = driver.findElement(By.className("breadcrumb"));
+        Assert.assertEquals(breadcrumb.findElement(By.linkText("Get involved")).getText(), "Get involved");
+        driver.findElement(By.id("edit-field-shop-geocode-latlon")).sendKeys("London");
+        driver.findElement(By.id("edit-submit-shop-listing")).click();
+        Assert.assertEquals(driver.findElement(By.className("shop-search-phone")).getText().trim(), "0207 4874986");
+
+        // Teardown
+        Thread.sleep(1000);
+        driver.quit();
+    }
+}
